28b0b39bdf89b2a0d3f6c92dc601475dafda2ada
cuteribs <ericfine1981@live.com>
2021-05-20 13:27:23 +0800

    Update temp.js

--- temp.js
+++ temp.js
@@ -7,59 +7,60 @@ const TMB_FOLDER = 'thumbnails';
 const TMB_WIDTH = 400;
 const INDEX_FILE = 'index.json';
 const API_URL = 'https://r5ea.z7.web.core.windows.net/images.json';
-let maxCommits = 100;
+const MAX_COMMITS = process.env.MAX_COMMITS || 100;
+const IMAGE_ON = process.env.IMAGE_ON || false;
+const TMB_ON = process.env.TMB_ON || false;
 
 async function getBingDailyList() {
+	let commits = MAX_COMMITS;
 	let isDirty = false;
-	let indexes = [];
+	let records = [];
 
-	if (fs.existsSync(INDEX_FILE)) indexes = JSON.parse(fs.readFileSync(INDEX_FILE));
+	if (fs.existsSync(INDEX_FILE)) {
+		records = JSON.parse(fs.readFileSync(INDEX_FILE));
+	}
 
 	const result = await request(API_URL);
 
-	for (const image of result.data) {
-		if(maxCommits <= 0) break;
-
-		info = {
-			date: image.date,
-			fileName: `OHR.${image.fileName}_UHD.jpg`,
-			title: image.title,
-			desc: image.desc
-		};
-
-		console.log(info.fileName, indexes.find((i) => i.fileName == info.fileName));
-		if (!indexes.find((i) => i.fileName == info.fileName)) {
-			indexes.push(info);
-			isDirty = true;
-		}
-
+	for (const info of result.data) {
+		if (commits <= 0) break;
 		await downloadImage(info);
+
+		if (!records.find((r) => r.fileName == info.fileName)) {
+			records.push(info);
+			isDirty = true;
+			commits--;
+		}
 	}
 
-	if (isDirty) {
-		console.log(`update: ${INDEX_FILE}`);
-		fs.writeFileSync(INDEX_FILE, JSON.stringify(indexes, null, '\t'));
-	}
+	if (isDirty) fs.writeFileSync(INDEX_FILE, JSON.stringify(records, null, '\t'));
 }
 
 async function downloadImage(info) {
-	const url = `https://www.bing.com/th?id=${info.fileName}`;
+	if (IMAGE_ON) {
+		const url = `https://www.bing.com/th?id=${info.fileName}`;
+		const path = `${IMAGE_FOLDER}/${info.fileName}`;
 
-	if (!fs.existsSync(IMAGE_FOLDER)) fs.mkdirSync(IMAGE_FOLDER);
+		if (!fs.existsSync(IMAGE_FOLDER)) fs.mkdirSync(IMAGE_FOLDER);
 
-	const imagePath = `${IMAGE_FOLDER}/${info.fileName}`;
-	const thumbPath = `${TMB_FOLDER}/${info.fileName}`;
+		if (!fs.existsSync(path)) {
+			const res = await request(url, { responseType: 'stream' });
+			res.data.pipe(fs.createWriteStream(path));
+			res.data.on('end', () => console.log(`🖼 image downloaded: ${path} 🖼`));
+		}
+	}
 
-	if (fs.existsSync(imagePath)) {
-		createThumbnail(imagePath, thumbPath);
-	} else {
-		const res = await request(url, { responseType: 'stream' });
-		res.data.pipe(fs.createWriteStream(imagePath));
-		res.data.on('end', () => {
-			maxCommits -= 1;
-			console.log(`image downloaded: ${imagePath}`);
-			createThumbnail(imagePath, thumbPath);
-		});
+	if (TMB_ON) {
+		const url = `https://www.bing.com/th?id=${info.fileName}&w=400&h=225`;
+		const path = `${TMB_FOLDER}/${info.fileName}`;
+
+		if (!fs.existsSync(TMB_FOLDER)) fs.mkdirSync(TMB_FOLDER);
+
+		if (!fs.existsSync(path)) {
+			const res = await request(url, { responseType: 'stream' });
+			res.data.pipe(fs.createWriteStream(path));
+			res.data.on('end', () => console.log(`🎞 thumbnail downloaded: ${path} 🎞`));
+		}
 	}
 }
 
@@ -79,6 +80,33 @@ async function request(url, options) {
 		responseType: (options && options.responseType) || 'JSON'
 	});
 	return res;
+}
+
+async function sendNotification(list) {
+	if (!list || list.length == 0) return;
+
+	if (!SERVER_J) return;
+
+	const title = 'Bing 每日壁纸收集完成';
+	const content = list.map((item) => {
+		return `
+# ${item.title}
+
+## ${item.date}
+
+[![${item.title}](https://www.bing.com/th?id=${item.fileName}&w=400&h=225)](https://www.bing.com/th?id=${infofileName})
+
+${item.desc}
+------
+`;
+	});
+
+	const url = `https://sctapi.ftqq.com/${SERVER_J}.send`;
+	const body = new URLSearchParams();
+	body.set('title', title);
+	body.set('desp', content);
+	await request(url, { method: 'POST', body });
+	console.log('💌 notification sent 💌');
 }
 
 getBingDailyList();
